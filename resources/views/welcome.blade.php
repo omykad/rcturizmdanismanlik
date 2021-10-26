8b95e1affedf9868fe18c93da19ddf5f280afced
Omer <kadioglufomer@gmail.com>
2021-10-27 10:21:53 +1100

    babussssssum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <meta content="" name="keywords">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/style.css?2" rel="stylesheet">
     <link href="/js/aos/aos.css" rel="stylesheet">
     <link href="/js/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/js/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -25,7 +25,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <div class="logo">
-        <h1><a href="/">RC DANISMANLIK</a></h1>
+        <h1><a href="/">RC</a></h1>
       </div>
 
       <nav id="navbar" class="navbar">
@@ -173,145 +173,99 @@
     </section><!-- End Counts Section -->
 
     <!-- ======= Services Section ======= -->
-    <section id="services" class="services">
+    <section id="services" class="features">
       <div class="container">
 
         <div class="section-title" data-aos="fade-up">
           <h2>Hizmetlerimiz</h2>
         </div>
 
-        <div class="row">
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4 class="title"><a href="">Fikir ve Görüşler  Konsept Geliştirmeler</a></h4>
-              <p class="description">
-              </p>
-            </div>
+          <div class="row" data-aos="fade-up" data-aos-delay="300">
+              <div class="col-lg-3 col-md-4">
+                  <div class="icon-box">
+                      <i class="ri-store-line" style="color: #ffbb2c;"></i>
+                      <h3><a href="">Personel Sağlama
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+                  <div class="icon-box">
+                      <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+                      <h3><a href="">Calisma Sistemi Kurulması
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+                  <div class="icon-box">
+                      <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
+                      <h3><a href="">Menu olusturulmasi</a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+                  <div class="icon-box">
+                      <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
+                      <h3><a href="">Maliyet çalışması ve kontrolü
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-database-2-line" style="color: #47aeff;"></i>
+                      <h3><a href="">Tedarikçilerle anlaşmalar
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
+                      <h3><a href="">Yerlesim duzeni
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
+                      <h3><a href="">Hizmet Kalite ve Standartlar Kontrolu
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
+                      <h3><a href="">Isyeri projelendirme ve kuruluşu
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-anchor-line" style="color: #b2904f;"></i>
+                      <h3><a href="">Butik otel ve üzeri işletmelere eğitim
+                          </a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-disc-line" style="color: #b20969;"></i>
+                      <h3><a href="">Moton Ideal</a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-base-station-line" style="color: #ff5828;"></i>
+                      <h3><a href="">Verdo Park</a></h3>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4">
+                  <div class="icon-box">
+                      <i class="ri-fingerprint-line" style="color: #29cc61;"></i>
+                      <h3><a href="">Flavor Nivelanda</a></h3>
+                  </div>
+              </div>
           </div>
 
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-              <div class="icon"><i class="bx bx-file"></i></div>
-              <h4 class="title"><a href="">Iş Planlama ve Konsept Oluşturma
-                  </a></h4>
-              <p class="description"></p>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-              <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4 class="title"><a href="">Menü ve Tasarımlar
-                  </a></h4>
-              <p class="description"></p>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
-              <div class="icon"><i class="bx bx-world"></i></div>
-              <h4 class="title"><a href="">Yatırım Faaliyetleri
-                  </a></h4>
-              <p class="description"></p>
-            </div>
-          </div>
-
-        </div>
 
       </div>
     </section><!-- End Services Section -->
-
-
-    <!-- ======= Features Section ======= -->
-    <section id="features" class="features">
-      <div class="container">
-        <div class="row" data-aos="fade-up" data-aos-delay="300">
-          <div class="col-lg-3 col-md-4">
-            <div class="icon-box">
-              <i class="ri-store-line" style="color: #ffbb2c;"></i>
-              <h3><a href="">Personel Sağlama
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
-            <div class="icon-box">
-              <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
-              <h3><a href="">Calisma Sistemi Kurulması
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
-            <div class="icon-box">
-              <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
-              <h3><a href="">Menu olusturulmasi</a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
-            <div class="icon-box">
-              <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
-              <h3><a href="">Maliyet çalışması ve kontrolü
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-database-2-line" style="color: #47aeff;"></i>
-              <h3><a href="">Tedarikçilerle anlaşmalar
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
-              <h3><a href="">Yerlesim duzeni
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
-              <h3><a href="">Hizmet Kalite ve Standartlar Kontrolu
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
-              <h3><a href="">Isyeri projelendirme ve kuruluşu
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-anchor-line" style="color: #b2904f;"></i>
-              <h3><a href="">Butik otel ve üzeri işletmelere eğitim
-                  </a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-disc-line" style="color: #b20969;"></i>
-              <h3><a href="">Moton Ideal</a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-base-station-line" style="color: #ff5828;"></i>
-              <h3><a href="">Verdo Park</a></h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-4 mt-4">
-            <div class="icon-box">
-              <i class="ri-fingerprint-line" style="color: #29cc61;"></i>
-              <h3><a href="">Flavor Nivelanda</a></h3>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End Features Section -->
-
 
     <!-- ======= Team Section ======= -->
     <section id="team" class="team section-bg">
@@ -349,7 +303,7 @@
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch" >
             <div class="member m-auto" data-aos="fade-up" data-aos-delay="300">
               <div class="member-img">
-                <img src="/img/team/team-3.jpg" class="img-fluid" style="height: 400px;width:300px;object-fit: cover;" alt="">
+                <img src="/img/candan.jpeg" class="img-fluid" style="height: 400px;width:300px;object-fit: cover;" alt="">
                 <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -384,36 +338,33 @@
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-5">
             <i class="ri-question-line"></i>
-            <h4>Non consectetur a erat nam at lectus urna duis?</h4>
+              <h4><b>Yatırımlarımız ne kadar sürede geri döner?</b></h4>
           </div>
           <div class="col-lg-7">
             <p>
-              Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-            </p>
+                Proje yatırımlar piyasa ve rekabet  koşullarıda göz önüne alınarak 3 yıl ve üzeri sürede geri dönmesi beklenmektedir.</p>
           </div>
         </div><!-- End F.A.Q Item-->
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
           <div class="col-lg-5">
             <i class="ri-question-line"></i>
-            <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+              <h4><b>Kurulmuş işletmelere hizmet veriyormusunuz?</b></h4>
           </div>
           <div class="col-lg-7">
             <p>
-              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
-            </p>
+                Faaliyetteki  işletmelerde yaşanan  veya beklenen ciroların düşük olmasından kaynaklı tüm problemlere çözüm ortağınız olacağız.             </p>
           </div>
         </div><!-- End F.A.Q Item-->
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
           <div class="col-lg-5">
             <i class="ri-question-line"></i>
-            <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+              <h4><b>İşletmeye desteğiniz ne kadar sürüyor?</b></h4>
           </div>
           <div class="col-lg-7">
             <p>
-              Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
-            </p>
+                Proje işletmelerde faaliyete başladıktan sonra 1 yıl süre içinde rütin kontrollerimiz devam ediyor. </p>
           </div>
         </div><!-- End F.A.Q Item-->
 
@@ -433,10 +384,9 @@
 
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
             <div class="contact-about">
-              <h3>RC Turizm</h3>
               <p>Gastronomi ve turizm alanındaki 45 yıllık profesyonel deneyimlerimizi sektör içindeki restaurantlar, cafeler ve oteller
                   olmak üzere faaliyete başlamış ancak istenilen seviyeye ulaşamamış olan veya yeni kurulacak  işletmelere proje aşamasından başlayarak
-                  A'dan  Z'ye danışmanlık hizmeti verebilmektir
+                  A'dan  Z'ye danışmanlık hizmeti veriyoruz.
               </p>
               <div class="social-links">
                 <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
@@ -451,12 +401,12 @@
             <div class="info">
               <div>
                 <i class="ri-map-pin-line"></i>
-                <p>A108 Adam Street<br>Izmir</p>
+                <p>Izmir</p>
               </div>
 
               <div>
                 <i class="ri-mail-send-line"></i>
-                <p>info@example.com</p>
+                <p>info@rcturizmdanismanlik.com</p>
               </div>
 
               <div>
@@ -470,16 +420,16 @@
           <div class="col-lg-5 col-md-12" data-aos="fade-up" data-aos-delay="300">
             <form action="../../../../../Users/tasho/Desktop/Vesperr/forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Isminiz" required>
               </div>
               <div class="form-group">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Mail Adresiniz" required>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Konu Basligi" required>
               </div>
               <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                <textarea class="form-control" name="message" rows="5" placeholder="Mesajiniz" required></textarea>
               </div>
               <div class="my-3">
                 <div class="loading">Loading</div>
