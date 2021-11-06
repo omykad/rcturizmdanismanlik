17806489e75c4d03e60f300aa1ba92e2f71fef8f
Omer <kadioglufomer@gmail.com>
2021-11-06 19:53:14 +1100

    v13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
   <meta content="" name="keywords">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/style.css?11" rel="stylesheet">
+    <link href="/css/style.css?13" rel="stylesheet">
     <link href="/js/aos/aos.css" rel="stylesheet">
     <link href="/js/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/js/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -57,7 +57,7 @@
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-          <img src="/img/first.svg" class="img-fluid animated" alt="">
+          <img src="/img/first.svg" class="img-fluid animated" alt="rc turizm danismanlik">
         </div>
       </div>
     </div>
@@ -74,28 +74,28 @@
           </div>
         <div class="row justify-content-center" >
 
-          <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/sheraton.png" class="img-fluid" alt="" data-aos="zoom-in">
+          <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/sheraton.png" class="img-fluid" alt="Sheraton Hotel" data-aos="zoom-in">
           </div>
 
-          <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/hilton.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-delay="100">
+          <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/hilton.png" class="img-fluid" alt="Hilton Izmir" data-aos="zoom-in" data-aos-delay="100">
           </div>
 
-          <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/nowness.svg" class="img-fluid" style="transform: scale(1.2);" alt="" data-aos="zoom-in" data-aos-delay="200">
+          <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/nowness.svg" class="img-fluid" style="transform: scale(1.2);" alt="Nowness Hotel" data-aos="zoom-in" data-aos-delay="200">
           </div>
 
-          <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/kaya.png" class="img-fluid" alt="" style="transform: scale(1.3);" data-aos="zoom-in" data-aos-delay="300">
+          <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/kaya.png" class="img-fluid" alt="Kaya Otel" style="transform: scale(1.3);" data-aos="zoom-in" data-aos-delay="300">
           </div>
 
-            <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/etap.png" class="img-fluid" alt="" style="transform: scale(0.9);"  width="0.5em" data-aos="zoom-in" data-aos-delay="400">
+            <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/etap.png" class="img-fluid" alt="Etap Hotel" style="transform: scale(0.9);"  width="0.5em" data-aos="zoom-in" data-aos-delay="400">
           </div>
 
-            <div class="col-lg-2 col-md-4 col-12 pb-3 pb-sm-0">
-            <img src="/img/grandankara.svg" class="img-fluid" style="transform: scale(1.6);" alt="" data-aos="zoom-in" data-aos-delay="500">
+            <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
+            <img src="/img/grandankara.svg" class="img-fluid" style="transform: scale(1.6);" alt="Grand Ankara Hotel" data-aos="zoom-in" data-aos-delay="500">
           </div>
 
         </div>
@@ -160,9 +160,11 @@
 
           <div class="col-xl-7 d-flex align-items-stretch pt-4 pt-xl-0" data-aos="fade-left" data-aos-delay="300">
             <div class="content d-flex flex-column justify-content-center">
+
               <div class="row">
-                <div class="col-md-6 d-md-flex align-items-md-stretch">
-                  <div class="count-box">
+
+                  <div class="col-md-6 d-md-flex align-items-md-stretch">
+                      <div class="count-box">
                     <i class="bi bi-emoji-smile"></i>
                       <p><strong>Yüzbinlerce kişinin</strong> memnuniyetini sağladık.</p>
                   </div>
@@ -207,7 +209,7 @@
 
           <div class="row" data-aos="fade-up" data-aos-delay="300">
 
-              <div class="col-lg-3 col-md-4 mt-3 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                       <i class="ri-store-line" style="color: #ffbb2c;"></i>
                       <h3>İşyeri projelendirme ve kuruluşu
@@ -220,7 +222,7 @@
                   </div>
               </div>
 
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box"  type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
                       <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
                       <h3>Bütçe ve maliyet oluşturulması
@@ -232,7 +234,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample3">
                       <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
                       <h3>İşyerinin Swot analizi</h3> <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
@@ -243,7 +245,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box " type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample4" aria-expanded="false" aria-controls="collapseExample4">
                       <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
                       <h3>Tedarikçilerle anlaşmalar
@@ -255,7 +257,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample5" aria-expanded="false" aria-controls="collapseExample5">
                       <i class="ri-database-2-line" style="color: #47aeff;"></i>
                       <h3>Butik otel ve üzeri işletmelere eğitim
@@ -267,7 +269,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample6" aria-expanded="false" aria-controls="collapseExample6">
                       <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
                       <h3>Hizmet kalite ve standardlarını oluşturmak
@@ -279,7 +281,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample7" aria-expanded="false" aria-controls="collapseExample7">
                       <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
                       <h3>İşletmeye personel sağlama
@@ -291,7 +293,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample8" aria-expanded="false" aria-controls="collapseExample8">
                       <i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
                       <h3>Satış pazarlama ve reklam
@@ -303,7 +305,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box " type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample9" aria-expanded="false" aria-controls="collapseExample9">
                       <i class="ri-anchor-line" style="color: #b2904f;"></i>
                       <h3>Konsept ve Menü
@@ -315,7 +317,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample10" aria-expanded="false" aria-controls="collapseExample10">
                       <i class="ri-disc-line" style="color: #b20969;"></i>
                       <h3>Gıda güvenliği</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
@@ -326,7 +328,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample11" aria-expanded="false" aria-controls="collapseExample11">
                       <i class="ri-base-station-line" style="color: #ff5828;"></i>
                       <h3>Sosyal medya yönetimi</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
@@ -337,7 +339,7 @@
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3 col-md-4 mt-4 position-relative">
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
                   <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample12" aria-expanded="false" aria-controls="collapseExample12">
                       <i class="ri-fingerprint-line" style="color: #29cc61;"></i>
                       <h3>İnsan kaynakları</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
@@ -348,6 +350,55 @@
                       </div>
                   </div>
               </div>
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
+                  <div class="icon-box " type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample13" aria-expanded="false" aria-controls="collapseExample13">
+                      <i class="ri-knife-line" style="color: #ffa76e;"></i>
+                      <h3>Mutfak Danismanligi
+                         </h3> <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
+                  </div>
+                  <div class="collapse" id="collapseExample13">
+                      <div class="card card-body border-0">
+                          Gastronomi ile ilgili tum evrensel bilgi ve birikimlerimiz mutfaginizin gucunu arttirir.
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
+                  <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample14" aria-expanded="false" aria-controls="collapseExample14">
+                      <i class="ri-service-fill" style="color: #ff5828;"></i>
+                      <h3>Servis Danismanligi</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
+                  </div>
+                  <div class="collapse" id="collapseExample14">
+                      <div class="card card-body border-0">
+                          Enternasyonel yiyecek ececek genel standartlari ve kurallari personelinizin gelisimini ve musterilerinizin memnuniyetini saglar.
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
+                  <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample15" aria-expanded="false" aria-controls="collapseExample15">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" color="#e361ff" fill="currentColor" class="bi bi-cup-straw" viewBox="0 0 16 16">
+                          <path d="M13.902.334a.5.5 0 0 1-.28.65l-2.254.902-.4 1.927c.376.095.715.215.972.367.228.135.56.396.56.82 0 .046-.004.09-.011.132l-.962 9.068a1.28 1.28 0 0 1-.524.93c-.488.34-1.494.87-3.01.87-1.516 0-2.522-.53-3.01-.87a1.28 1.28 0 0 1-.524-.93L3.51 5.132A.78.78 0 0 1 3.5 5c0-.424.332-.685.56-.82.262-.154.607-.276.99-.372C5.824 3.614 6.867 3.5 8 3.5c.712 0 1.389.045 1.985.127l.464-2.215a.5.5 0 0 1 .303-.356l2.5-1a.5.5 0 0 1 .65.278zM9.768 4.607A13.991 13.991 0 0 0 8 4.5c-1.076 0-2.033.11-2.707.278A3.284 3.284 0 0 0 4.645 5c.146.073.362.15.648.222C5.967 5.39 6.924 5.5 8 5.5c.571 0 1.109-.03 1.588-.085l.18-.808zm.292 1.756C9.445 6.45 8.742 6.5 8 6.5c-1.133 0-2.176-.114-2.95-.308a5.514 5.514 0 0 1-.435-.127l.838 8.03c.013.121.06.186.102.215.357.249 1.168.69 2.438.69 1.27 0 2.081-.441 2.438-.69.042-.029.09-.094.102-.215l.852-8.03a5.517 5.517 0 0 1-.435.127 8.88 8.88 0 0 1-.89.17zM4.467 4.884s.003.002.005.006l-.005-.006zm7.066 0-.005.006c.002-.004.005-.006.005-.006zM11.354 5a3.174 3.174 0 0 0-.604-.21l-.099.445.055-.013c.286-.072.502-.149.648-.222z"/>
+                      </svg>
+                      <h3> &nbsp; Bar Danismanligi</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
+                  </div>
+                  <div class="collapse" id="collapseExample15">
+                      <div class="card card-body border-0">
+                         Bar tasarim ve envanterini duzenleme, barmen-barmain egitimleri, alkol ve kokteyl menu olusturma.
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-4 mt-4 position-relative px-1">
+                  <div class="icon-box" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample16" aria-expanded="false" aria-controls="collapseExample16">
+                      <i class="ri-global-line" style="color: #e80368;"></i>
+                      <h3>Marka iletişimi ve yönetimi</h3>  <i class="dropdown-toggle fs-6 position-absolute" style="right:5px;color: #333;"></i>
+                  </div>
+                  <div class="collapse" id="collapseExample16">
+                      <div class="card card-body border-0">
+
+                      </div>
+                  </div>
+              </div>
+
+
           </div>
 
 
@@ -429,18 +480,20 @@
           </div>
         </div><!-- End F.A.Q Item-->
 
-        <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-          <div class="col-lg-5">
-            <i class="ri-question-line"></i>
-              <h4><b>Kurulmuş işletmelere hizmet veriyormusunuz?</b></h4>
-          </div>
-          <div class="col-lg-7">
-            <p>
-                Faaliyetteki  işletmelerde yaşanan  veya beklenen ciroların düşük olmasından kaynaklı tüm isteklerinize yardımcıyız.</p>
-          </div>
-        </div><!-- End F.A.Q Item-->
+          <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+              <div class="col-lg-5">
+                  <i class="ri-question-line"></i>
+                  <h4><b>Kurulmuş i̇şletmelere hi̇zmet veri̇yor musunuz??</b></h4>
+              </div>
+              <div class="col-lg-7">
+                  <p>
+                      Evet! Faaliyetteki işletmelerde yaşanan veya beklenen ciroların düşük olmasından kaynaklı
+                      tüm isteklerinize çözüm ortağıyız. </p>
+              </div>
+          </div><!-- End F.A.Q Item-->
 
-        <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+
+          <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
           <div class="col-lg-5">
             <i class="ri-question-line"></i>
               <h4><b>İşletmeye desteğiniz ne kadar sürüyor?</b></h4>
@@ -451,17 +504,6 @@
           </div>
         </div><!-- End F.A.Q Item-->
 
-          <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-          <div class="col-lg-5">
-            <i class="ri-question-line"></i>
-              <h4><b>Kurulmuş i̇şletmelere hi̇zmet veri̇yor musunuz??</b></h4>
-          </div>
-          <div class="col-lg-7">
-            <p>
-                Evet! Faaliyetteki işletmelerde yaşanan veya beklenen ciroların düşük olmasından kaynaklı
-                tüm isteklerinize çözüm ortağıyız. </p>
-          </div>
-        </div><!-- End F.A.Q Item-->
 
 
       </div>
