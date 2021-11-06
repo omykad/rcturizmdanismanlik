f87f68e8203792194e2dc18ff975dfedc473a268
Omer <kadioglufomer@gmail.com>
2021-11-06 23:39:33 +1100

    v15

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" href="/img/rc400.png" style="width: 150px; height: 100px">
-    <link rel="apple-touch-icon" type="image/png" href="/img/rc400.png" style="width: 150px; height: 100px">
+    <link rel="icon" type="image/png" href="/img/rc400.png" sizes="32x32">
+    <link rel="apple-touch-icon" type="image/png" href="/img/rc400.png" sizes="32x32">
     <title>RC Turizm</title>
   <meta content="Gastronomi ve turizm alanındaki 40 yıllık profesyonel deneyimlerimizi sektör içindeki restaurantlar&cafeler ve oteller olmak üzere faaliyete başlamış ancak istenilen seviyeye ulaşamamış olan veya yeni kurulacak işletmelere proje aşamasından başlayarak A'dan Z'ye danışmanlık hizmeti vermekteyiz." name="description">
   <meta content="izmir, istanbul, danismanlik, turizm, cafe, otel" name="keywords">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/style.css?14" rel="stylesheet">
+    <link href="/css/style.css?15" rel="stylesheet">
     <link href="/js/aos/aos.css" rel="stylesheet">
     <link href="/js/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/js/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
