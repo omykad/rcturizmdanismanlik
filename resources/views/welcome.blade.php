a1d0a19972437194cf68e01f13a6d8e8f25d05f7
Omer <kadioglufomer@gmail.com>
2021-11-10 22:14:54 +1100

    v17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
   <meta content="Gastronomi ve turizm alanındaki 40 yıllık profesyonel deneyimlerimizi sektör içindeki restaurantlar&cafeler ve oteller olmak üzere faaliyete başlamış ancak istenilen seviyeye ulaşamamış olan veya yeni kurulacak işletmelere proje aşamasından başlayarak A'dan Z'ye danışmanlık hizmeti vermekteyiz." name="description">
   <meta content="izmir, istanbul, danismanlik, turizm, cafe, otel" name="keywords">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/style.css?16" rel="stylesheet">
+    <link href="/css/style.css?17" rel="stylesheet">
     <link href="/js/aos/aos.css" rel="stylesheet">
     <link href="/js/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/js/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -87,8 +87,8 @@
             <img src="/img/nowness.png" class="img-fluid" style="transform: scale(1.2);" alt="Nowness Hotel" data-aos="zoom-in" data-aos-delay="200">
           </div>
 
-          <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
-            <img src="/img/kaya2.png" class="img-fluid" alt="Kaya Otel" style="transform: scale(1.3);" data-aos="zoom-in" data-aos-delay="300">
+          <div class="col-lg-2 col-md-4 col-12 p-0 pb-sm-0">
+            <img src="/img/kaya2.png" class="img-fluid" alt="Kaya Otel" style="transform: scale(2);" data-aos="zoom-in" data-aos-delay="300">
           </div>
 
             <div class="col-lg-2 col-md-4 col-12 p-3 pb-sm-0">
